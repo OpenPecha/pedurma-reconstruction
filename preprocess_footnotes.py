5585798583abbeb75d3689adde28810a7eba371c
ngawangtrinley <ngawangtrinley@gmail.com>
2020-05-25 15:07:43 +0800

    narthang notes preprocessing done

--- preprocess_footnotes.py
+++ preprocess_footnotes.py
@@ -14,14 +14,12 @@ def derge_page_increment(p_num):
     return f'[{page}{side}]'
 
 
-def preprocessGoogleNotes(filename):
+def preprocessGoogleNotes(text):
     """
     this cleans up all note markers
     :param text: plain text
     :return: cleaned text
     """
-    text = filename.read_text(encoding='utf-8')
-
     patterns = [
         # delete tibetan numbers
         # ['[༠-༩]', ''],
@@ -81,7 +79,7 @@ def preprocessGoogleNotes(filename):
         # delete note markers
         # ['<', ''],
 
-        ['»\n', '»'],  # to put all the notes split on two lines on a single one
+        ['»\n', '»'],  # put all the notes split on two lines on a single one
         ['། །\n', '།\n'],
         ]
 
@@ -93,15 +91,19 @@ def preprocessGoogleNotes(filename):
 »འཁྲང་། ༄༅། «གཡུང་»
 '''
 
-def preprocessNamselNotes(filename):
+def preprocessNamselNotes(text):
     """
     this cleans up all note markers
     :param text: plain text
     :return: cleaned text
     """
-    text = filename.read_text(encoding='utf-8')
 
     patterns = [
+        # normalize single zeros '༥༥་' --> '༥༥༠'
+        ['([༠-༩])[་༷]', '\g<1>༠'],
+        # normalize double zeros '༧༷་' --> '༧༠༠'
+        ['༠[་༷]', '༠༠'],
+        ['༠[་༷]', '༠༠'],
         # normalize punct
         ['\r', '\n'],
         ['༑', '།'],
@@ -116,9 +118,6 @@ def preprocessNamselNotes(filename):
         ['་\s*', '་'],
         ['་\s*\n', '་'],
         ['་+', '་'],
-        # tag pedurma page numbers #<vol-page>#
-        ['([0-9]{1,3})\D་?([0-9]+)', '\n#\g<1>-\g<2>#\n'],    # well formated
-        # ['([^\d#-])([0-9]{3,10})', '\g<1>\n#\g<2>#\n'],    # not well formated
         # delete tibetan numbers
         # ['[༠-༩]', ''],
         # headers ++<header>++
@@ -128,6 +127,11 @@ def preprocessNamselNotes(filename):
         ['(\{[^\}]+?) (.+?\})', '\g<1>_\g<2>'],     # deal with spaces in special notes
         ['(\{[^\}]+?) (.+?\})', '\g<1>_\g<2>'],     # deal with spaces in special notes
         ['(\{[^\}]+?) (.+?\})', '\g<1>_\g<2>'],     # deal with spaces in special notes
+        # normalize and tag page numbers '73ཝ་768' --> ' <p73-768> '
+        ['([0-9]+?)[ཝ—-]་?([0-9]+)', ' <p\g<1>-\g<2>> '],
+        # tag page references '༡༤༥ ①' --> <p༡༤༥> ①'   
+        # ཉེ༠སྟེཏུཉེཐོ)༦)
+        [' ?([༠-༩]+?)(\s[①-⓪༠-༩ ཿ])', ' \n<r\g<1>>\g<2>'],   # basic page ref
         # normalize edition marks «<edition>»
         ['〈〈?', '«'],      
         ['〉〉?', '»'], 
@@ -147,18 +151,26 @@ def preprocessNamselNotes(filename):
         ['([^»]+«) ', '\g<1>» '],
         ['([^»]+«)-', '\g<1>»-'],
         ['(«[^་]+?་)([^»])', '\g<1>»\g<2>'],
-        # tag note markers \<<note>\>
-        ['([ཤཀག།] )([^།»\}]+)«', '\g<1>\n<\g<2>>«'],
+        ['(»[^«]+?)»', '\g<1>'],    # fix extra
+        # tag note markers <note>
+        ['([ཤཀག།\n] )([^།»\}<>]+)«', '\g<1>\n<m\g<2>>«'],
         ['<\n(\{.+?)>«', '\g<1>«'],     # fix special note markers
-        ['(\s?[①-㊿]+)«', '\n<\g<1>>«'],
+        ['(\s?[①-㊿༠-༩]+)«', '\n<m\g<1>>«'],
+        ['\n<m([^ >]+?[ཤཀག།] )', '\g<1>\n<m'],   # fix multi-syls A
+        ['\n([^།»\{}<>]+)«', '\n<m\g<1>>«'], # fix ref at line start
+        ['> ?([^>«»]+?)«', '>\n<m\g<1>>«'],   # fix ref + marker
+        ['([^\n])<r', '\g<1>\n<r'],   # fix inline ref 
+        ['\s([^<>«» ]+?)«', ' \n<m\g<1>>«'], # fix ?
+        ['«[^»]+?«ང་»', '«གཡུང་»'],    # fix g.yung
         # [' ([^ༀ-࿚]+)«', '\n<\g<1>>«'],  # catch ། @ «
-        # Normalize zero in page references
-        ['([༠-༩])[༷་]', '\g<1>༠'],
         # Add page references to first footnote marker
-        ['([༠-༩]+)([\n\s]*)<([\s]*①)', '\g<2><\g<1>\g<3>'],
+        # ['([༠-༩]+)([\n\s]*)<([\s]*①)', '\g<2><\g<1>\g<3>'],
 
-        ['»\n', '»'],  # to put all the notes split on two lines on a single one
+        ['»\n([^<])', '»\g<1>'],  # to put all the notes split on two lines on a single one
         ['། །\n', '།\n'],
+
+        ['(<[mpr])\n', '\g<1>'],
+        ['\n<m\s*>', ''],   # fix multi-syls B
         ]
 
 
@@ -175,9 +187,42 @@ def preprocessNamselNotes(filename):
 [^»> ]«
 '''
 
+def tag_page_references(content):
+
+    patterns = [
+        # normalize single zeros '༥༥་' --> '༥༥༠'
+        ['([༠-༩])[་༷]', '\g<1>༠'],
+        # normalize double zeros '༧༷་' --> '༧༠༠'
+        ['༠[་༷]', '༠༠'],
+        ['༠[་༷]', '༠༠'],
+        # tag page references '༡༤༥ ①' --> <p༡༤༥> ①'
+        [' ?([༠-༩]+?)(\s[①-⓪༠-༩])', ' <r\g<1>> \g<2>'],
+        ]
+
+    for p in patterns:
+        print(p)
+        content = re.sub(p[0], p[1], content)
+    return content
+
+def tag_page_numbers(content):
+
+    patterns = [
+        # normalize and tag page numbers '73ཝ་768' --> ' <p73-768> '
+        ['([0-9]+?)[ཝ—-]་?([0-9]+)', ' <p\g<1>-\g<2>> '],
+        ]
+
+    for p in patterns:
+        content = re.sub(p[0], p[1], content)
+    return content
+
 
 def process(dump, page_num):
-
+    patterns = [
+        # normalize punct
+        ['\r', '\n'],
+        ['༑', '།'],
+        ['།།', '། །'],
+        ]
     output = []
     pages = dump.split('\n\n')
     # print(pages)
@@ -199,16 +244,31 @@ def save(content, filename, tag):
 if __name__ == '__main__':
     # Path to the initial Google OCR file
     basePath = Path("./input/footnote_text")
+
     googlePath = basePath / 'googleOCR_text' / '73durchen-google.txt'
     namselPath = basePath / 'namselOCR_text' / '73durchen-namsel.txt'
     # derge page on which the text starts
     init_num = '[135a]'
 
-    # script steps
-    googlePrep = preprocessGoogleNotes(googlePath)
+    # Google footnotes
+    google_content = googlePath.read_text(encoding='utf-8')
+    # get text
+    googlePrep = preprocessGoogleNotes(google_content)
+    # get text
     save(googlePrep, googlePath, '_num')
-    googleProc = process(googlePrep, init_num)
+    # get text
+    # googleProc = process(googlePrep, init_num)
 
-    namselPrep = preprocessNamselNotes(namselPath)
+    # Namsel footnotes
+    namsel_content = namselPath.read_text(encoding='utf-8')
+    # isolate and normalize pedurma pages '73-23་' --> ' <p73-230> '
+    # namsel_content = tag_page_numbers(namsel_content)
+    # isolate and normalize pedurma page references '༢༣་' --> ' <r༢༣༠> '
+    # namsel_content = tag_page_references(namsel_content)
+    # save(namsel_content, namselPath, '_num')
+    # clean content
+    namselPrep = preprocessNamselNotes(namsel_content)
+    # write to file
     save(namselPrep, namselPath, '_num')
-    namselProc = process(namselPrep, init_num)
+    # get text
+    # namselProc = process(namselPrep, init_num)
